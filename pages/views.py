be5416cbeb9f8c637552cf247b1e93e9211fbb22
Svpa04 <sofi04122001@gmail.com>
2024-12-03 00:55:19 -0400

    about page

--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePagesView(TemplateView):
     template_name = "home.html"
-    
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
