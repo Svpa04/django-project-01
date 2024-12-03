4c89dc7d6a839f9da3a08893f574b2ff92b7fb5e
Svpa04 <sofi04122001@gmail.com>
2024-12-03 00:42:54 -0400

    minor changes

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import HomePagesView
 
 urlpatterns = [
-    path("", HomePagesView.as_view(), name="home"), 
+    path("", HomePagesView.as_view(), name="home"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,3 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePagesView(TemplateView):
     template_name = "home.html"
-    
